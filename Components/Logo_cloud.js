a824be418f38ca81b7a56527207b9895dc67ef5e
nandishbt <nandishbt2001@gmail.com>
2024-01-10 15:50:37 +0530

    breakpoint for tablet added

--- Components/Logo_cloud.js
+++ Components/Logo_cloud.js
@@ -4,15 +4,15 @@ import Image from 'next/image'
 
 const Logo_cloud = () => {
   return (
-    <div className='container-fluid h-40 flex items-center justify-center gap-5 '>
-        <img className="w-28" src='/Images/nuxt.png'/>
-        <img className="w-28" src='/Images/miro.png'/>
-        <img className="w-28" src='/Images/lottiefiles.png'/>
-        <img className="w-28" src='/Images/wise.png'/>
-        <img className="w-28" src='/Images/dribble.png'/>
-        <img className="w-28" src='/Images/kinsta.png'/>
-        <img className="w-28" src='/Images/angellist.png'/>
-        <img className="w-28" src='/Images/behance.png'/>
+    <div className='container h-40 flex items-center justify-center gap-5 md:gap-0 flex-wrap'>
+        <img className="w-28 md:w-18" src='/Images/nuxt.png'/>
+        <img className="w-28 md:w-18" src='/Images/miro.png'/>
+        <img className="w-28 md:w-18" src='/Images/lottiefiles.png'/>
+        <img className="w-28 md:w-18" src='/Images/wise.png'/>
+        <img className="w-28 md:w-18" src='/Images/dribble.png'/>
+        <img className="w-28 md:w-18" src='/Images/kinsta.png'/>
+        <img className="w-28 md:w-18" src='/Images/angellist.png'/>
+        <img className="w-28 md:w-18" src='/Images/behance.png'/>
         
 
     </div>
